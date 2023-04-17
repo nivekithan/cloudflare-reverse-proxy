6d7e3f065e585bd77d829db53f1c04ce7bcf9d4e
Nivekithan S <nivekithan363@gmail.com>
2023-04-17 21:20:19 +0530

    Code without kv

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import useReflare from "reflare";
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
+  // MAGIC_REVERSE_PROXY: KVNamespace;
   //
   // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
   // MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -23,40 +14,32 @@ export interface Env {
 }
 
 const WEBSITE_HOSTS = {
-  1: "reverse-proxy-project-1.pages.dev",
-  2: "reverse-proxy-remix-project-2.pages.dev",
+  magic: "magic.app",
+  epyc: "www.epyc.in",
 };
+
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const newRequest = getNewRequest(request);
+    const reflare = await useReflare({
+      provider: "static",
+      routeList: [
+        {
+          path: "/product/broadcast",
+          upstream: { domain: WEBSITE_HOSTS["magic"] },
+        },
+        {
+          path: "/*",
+          upstream: {
+            domain: WEBSITE_HOSTS["epyc"],
+          },
+        },
+      ],
+    });
 
-    const data = await fetch(newRequest.url, newRequest);
-
-    return data;
+    return reflare.handle(request);
   },
 };
-
-function isWebsite1(url: URL) {
-  return url.pathname.startsWith("/1/");
-}
-
-function getNewRequest(request: Request) {
-  const requestUrl = new URL(request.url);
-
-  const newUrl = new URL(request.url);
-
-  newUrl.host = isWebsite1(requestUrl) ? WEBSITE_HOSTS[1] : WEBSITE_HOSTS[2];
-
-  const newHeaders = new Headers(request.headers);
-
-  newHeaders.set("x-original-url", request.url);
-
-  return new Request(
-    newUrl.toString(),
-    new Request(request, { headers: newHeaders })
-  );
-}
